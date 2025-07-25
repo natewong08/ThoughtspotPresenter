8e4c8de70e7153d51b439e8cca58752c408091cb
Nathan Wong <natewong08@gmail.com>
2025-07-25 12:33:34 +0100

    Update env variable name to TS_USERNAME

--- backend/app.js
+++ backend/app.js
@@ -7,50 +7,50 @@ dotenv.config();
 const app = express();
 const port = 8080;
 app.use(
-	cors({
-		origin: "http://localhost:3000",
-	})
+  cors({
+    origin: "http://localhost:3000",
+  })
 );
 
 app.get("/thoughtspotToken", async (req, res) => {
-	// secret_key must be stored securely; here we assume the ThoughtSpot server name and secret_key are stored in env file
-	let thoughtspot_url = process.env.TS_SERVER_URI;
-	let thoughtspot_secret_key = process.env.TS_SECRET_KEY;
+  // secret_key must be stored securely; here we assume the ThoughtSpot server name and secret_key are stored in env file
+  let thoughtspot_url = process.env.TS_SERVER_URI;
+  let thoughtspot_secret_key = process.env.TS_SECRET_KEY;
 
-	// Short-time out for cookie-based login; make longer for cookieless
-	const timeoutInSec = 60;
+  // Short-time out for cookie-based login; make longer for cookieless
+  const timeoutInSec = 60;
 
-	// Assuming 'user_email' is the session value that matches the username in ThoughtSpot
-	let tsTokenUrl = thoughtspot_url + "api/rest/2.0/auth/token/full";
-	let tsTokenBody = {
-		username: process.env.USERNAME,
-		validity_time_in_sec: timeoutInSec,
-		auto_create: false,
-		secret_key: thoughtspot_secret_key,
-	};
-	// using Fetch to make HTTP request; you may use other package
-	const response = await fetch(tsTokenUrl, {
-		method: "POST",
-		body: JSON.stringify(tsTokenBody),
-		headers: {
-			Accept: "application/json",
-			"X-Requested-By": "ThoughtSpot",
-			"Content-Type": "application/json",
-		},
-	});
+  // Assuming 'user_email' is the session value that matches the username in ThoughtSpot
+  let tsTokenUrl = thoughtspot_url + "api/rest/2.0/auth/token/full";
+  let tsTokenBody = {
+    username: process.env.TS_USERNAME,
+    validity_time_in_sec: timeoutInSec,
+    auto_create: false,
+    secret_key: thoughtspot_secret_key,
+  };
+  // using Fetch to make HTTP request; you may use other package
+  const response = await fetch(tsTokenUrl, {
+    method: "POST",
+    body: JSON.stringify(tsTokenBody),
+    headers: {
+      Accept: "application/json",
+      "X-Requested-By": "ThoughtSpot",
+      "Content-Type": "application/json",
+    },
+  });
 
-	const results = await response.json();
-	res.send(results.token);
+  const results = await response.json();
+  res.send(results.token);
 });
 
 app.get("/inputs", (req, res) => {
-	const inputs = {
-		thoughtSpotHost: process.env.TS_SERVER_URI,
-		liveboardId: process.env.LIVEBOARD_ID,
-	};
-	res.send(inputs);
+  const inputs = {
+    thoughtSpotHost: process.env.TS_SERVER_URI,
+    liveboardId: process.env.LIVEBOARD_ID,
+  };
+  res.send(inputs);
 });
 
 app.listen(port, () => {
-	console.log("Backend server running...");
+  console.log("Backend server running...");
 });
--- start.sh
+++ start.sh
@@ -5,10 +5,10 @@ npm run start-frontend &
 
 # uncomment the command below based on your OS and comment out the others
 # for OSX machines
-open http://localhost:3000
+# open http://localhost:3000
 
 # for Windows machines
-# start http://localhost:3000
+start http://localhost:3000
 
 # for Linux machines
 # xdg-open http://localhost:3000
